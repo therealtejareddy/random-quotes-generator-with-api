e6d67882ecbb61cd87e40cec884bc48068b1b2de
Yuvateja <yuvatejareddy003@gmail.com>
2021-03-24 22:45:50 +0530

    first commit :rocket:

---
+++ random quotes/script.js
@@ -0,0 +1,17 @@
+const quote = document.querySelector('.quote');
+const author = document.querySelector('.author');
+const btn = document.querySelector('.btn');
+
+let updateQuote = async () => {
+    fetch('https://api.quotable.io/random')
+        .then(response => response.json())
+        .then(data => {
+            quote.textContent = data.content;
+            author.textContent = '- '+data.author;
+        })
+}
+document.addEventListener('DOMContentLoaded', updateQuote);
+btn.addEventListener('click', () => updateQuote());
+
+
+
