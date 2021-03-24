0567a86dde26fe37fc45a8b94868232150487f0f
Yuvateja <yuvatejareddy003@gmail.com>
2021-03-24 22:52:10 +0530

    Quotes :rocket:

--- random quotes/script.js
+++ random quotes/script.js
@@ -8,8 +8,8 @@ let updateQuote = async () => {
         .then(data => {
             quote.textContent = data.content;
             author.textContent = '- '+data.author;
-        })
-}
+        });
+};
 document.addEventListener('DOMContentLoaded', updateQuote);
 btn.addEventListener('click', () => updateQuote());
 
